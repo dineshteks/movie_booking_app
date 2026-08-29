069d18d99757fabfd9fdecc6f2b49a0cdb872e98
dinesh <dineshteks@gmail.com>
2026-08-29 12:25:26 +0530

    Merge remote repository with local project

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -84,13 +84,13 @@ function Navbar() {
               Movies
             </Link>
 
-            <Link
+            {/* <Link
               to="/theaters"
               onClick={() => setIsOpen(false)}
               className="hover:text-red-500 transition duration-300"
             >
               Theaters
-            </Link>
+            </Link> */}
 
             <Link
               to="/bookings"
